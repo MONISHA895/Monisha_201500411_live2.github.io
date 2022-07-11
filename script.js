b686afa0976ee29878e03cd3af5c9c27d423a6e5
“monisha” <monisha.ramani_cs20@gla.ac.in>
2022-07-11 21:08:42 +0530

    demo template

---
+++ script.js
@@ -0,0 +1,5 @@
+var myname = document.getElementsByClassName("name");
+myname.innerHTML = `<span>Monisha Moti Ramani</span>`;
+var college = document.getElementsByClassName();
+
+date_.value = new Date().getDate;
